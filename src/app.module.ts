c88ce1df11989eaef0ba3fdc261817873da2e3f8
titan00001 <gplay9792@gmail.com>
2021-12-30 16:46:02 +0530

    added nest-auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GoogleStrategy } from './google.strategy';
+import { AuthService, NestAuthModule } from 'nest-auth';
 
 @Module({
-  imports: [],
+  imports: [
+    NestAuthModule.register({
+      strategies: {
+        google: {
+          clientID: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_SECRET,
+          callbackURL: 'http://localhost:3000/google/redirect',
+          scope: ['email', 'profile'],
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService, GoogleStrategy],
+  providers: [AppService, GoogleStrategy, AuthService],
 })
 export class AppModule {}
