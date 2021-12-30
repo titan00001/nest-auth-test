20cddcb37d0057d21184c77aaa66c4dd8d2e84f4
titan00001 <gplay9792@gmail.com>
2021-12-30 18:39:16 +0530

    Added expt-1 branch

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GoogleStrategy } from './google.strategy';
-import { AuthService, NestAuthModule } from 'nest-auth';
+// import { GoogleStrategy } from './google.strategy';
+import { NestAuthModule } from 'nest-auth';
 
 @Module({
   imports: [
-    NestAuthModule.register({
-      strategies: {
-        google: {
-          clientID: process.env.GOOGLE_CLIENT_ID,
-          clientSecret: process.env.GOOGLE_SECRET,
-          callbackURL: 'http://localhost:3000/google/redirect',
-          scope: ['email', 'profile'],
-        },
-      },
-    }),
+    NestAuthModule,
+    // .register({
+    //   strategies: {
+    //     google: {
+    //       clientID: process.env.GOOGLE_CLIENT_ID,
+    //       clientSecret: process.env.GOOGLE_SECRET,
+    //       callbackURL: 'http://localhost:3000/google/redirect',
+    //       scope: ['email', 'profile'],
+    //     },
+    //   },
+    // }),
   ],
   controllers: [AppController],
-  providers: [AppService, GoogleStrategy, AuthService],
+  providers: [
+    AppService, 
+    // GoogleStrategy,
+  ],
 })
 export class AppModule {}
